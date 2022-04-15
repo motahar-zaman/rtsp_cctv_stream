28a6417675143a3883bba3ac241f05f9ef99b80f
zaman <motaharz95@gmail.com>
2022-04-16 04:35:29 +0600

    rtsp demo video play successfully

--- cctv_stream.py
+++ cctv_stream.py
@@ -1,7 +1,11 @@
 import cv2
 import imutils
 from imutils.video import VideoStream
-rtsp_url = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_175k.mov"
+rtsp_url = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4"
+# rtsp_url = "rtsp://FAruk:FAruk@45#!@202.4.125.250/media/video1"
+# rtsp_url = "rtsp://FAruk:FAruk@45#!@202.4.125.254:554/cam/realmonitor?channel=1&subtype=0"
+# rtsp_url = "rtsp://amberitltd:AmberIT!#1997@202.4.125.254:554/cam/realmonitor?channel=1&subtype=0"
+# rtsp_url = "rtsp://admin:@mber@it#1997@202.4.125.250/unicast/c2/s2/live"
 video_stream = VideoStream(rtsp_url).start()
 
 while True:
@@ -9,7 +13,7 @@ while True:
     if frame is None:
         continue
 
-    frame = imutils.resize(frame,width=1200)
+    frame = imutils.resize(frame, width=800)
     cv2.imshow('AsimCodeCam', frame)
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
