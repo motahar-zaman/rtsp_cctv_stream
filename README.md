d151c049977ae3f4d1b337e4db8b160ec2fcf79b
zaman <motaharz95@gmail.com>
2022-04-15 16:59:04 +0600

    initiate cctv_stream project

---
+++ README.md
@@ -0,0 +1,1 @@
+# rtsp_cctv_stream
---
+++ cctv_stream.py
@@ -0,0 +1,19 @@
+import cv2
+import imutils
+from imutils.video import VideoStream
+rtsp_url = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_175k.mov"
+video_stream = VideoStream(rtsp_url).start()
+
+while True:
+    frame = video_stream.read()
+    if frame is None:
+        continue
+
+    frame = imutils.resize(frame,width=1200)
+    cv2.imshow('AsimCodeCam', frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+video_stream.stop()
