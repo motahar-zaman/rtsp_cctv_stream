0c961fd580a9ad2fed8f10b303d9ddcb2bbc801b
zaman <motaharz95@gmail.com>
2022-04-26 04:24:27 +0600

    api structure to generate url for various types of ip camera and stream video using the url

--- cctv_stream.py
+++ cctv_stream.py
@@ -1,11 +1,13 @@
 import cv2
-from flask import Flask, render_template, Response
-rtsp_url = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4"
+from flask import Flask, render_template, Response, request
+
+# rtsp_url = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4"
+# rtsp_url = "rtsp://admin:@mber@it#1997@202.4.125.250/unicast/c2/s2/live"
 app = Flask(__name__)
 capture = cv2.VideoCapture(rtsp_url)
 
 
-def gen_frames():
+def gen_frames(rtsp_url):
     while True:
         success, frame = capture.read()  # read the camera frame
         if not success:
@@ -22,14 +24,37 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/camera_feed')
+@app.route('/camera_feed', methods=['GET', 'POST'])
 def camera_feed():
     return render_template('camera_stream.html')
 
 
-@app.route('/video_feed')
+@app.route('/video_feed', methods=['GET'])
 def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    rtsp_url = request.args['rtsp_url']
+
+    return Response(gen_frames(rtsp_url), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/video_feed', methods=['POST'])
+def rtsp_url():
+    url = ''
+    user_name = request.form['user_name']
+    password = request.form['password']
+    ip_address = request.form['ip_address']
+    port = request.form['port']
+    camera_type = request.form['camera_type']
+
+    if camera_type == 'dahua':
+        url = 'rtsp://' + user_name + ':' + password + '@' + ip_address + ':' + port + '/cam/realmonitor?channel=<channelNo>&subtype=<typeNo>'
+    elif camera_type == 'hikvision':
+        url = 'rtsp://' + user_name + ':' + password + '@' + ip_address + ':' + port + '/cam/realmonitor?channel=<channelNo>&subtype=<typeNo>'
+    elif camera_type == 'uniview':
+        url = 'rtsp://' + user_name + ':' + password + '@' + ip_address + ':' + port + '/cam/realmonitor?channel=<channelNo>&subtype=<typeNo>'
+    elif camera_type == 'axis':
+        url = 'rtsp://' + user_name + ':' + password + '@' + ip_address + ':' + port + '/cam/realmonitor?channel=<channelNo>&subtype=<typeNo>'
+
+    return url
 
 
 if __name__ == "__main__":
